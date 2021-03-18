774720467eca49eeee4465123bf19fd663c621f2
LukaHerrmann <lukajherrmann@gmail.com>
2021-03-18 16:54:33 +0100

    Commentregels toegevoegd.

--- General/GrapicalInterface.py
+++ General/GrapicalInterface.py
@@ -2,6 +2,8 @@ from tkinter import *
 
 
 def makewindow(windowwidth, windowheight, windowtitle, bgcolor):
+    '''Deze functie maakt een scherm met meegegeven afmetingen en plaatst deze in het midden van het scherm
+    en returnt deze voor verder gebruik'''
     root = Tk()
     # De breedte en hoogte van het scherm ophalen
     screenwidth = root.winfo_screenwidth()
@@ -17,6 +19,8 @@ def makewindow(windowwidth, windowheight, windowtitle, bgcolor):
 
 
 def makelabel(root, x, y, text, font, textcolor, bgcolor, anchor):
+    '''Deze functie maakt een stukje text op basis van de meegegeven variabelen en returnt deze voor
+    verder gebruik'''
     label = Label(root,
                   text=text,
                   font=font,
@@ -27,6 +31,8 @@ def makelabel(root, x, y, text, font, textcolor, bgcolor, anchor):
 
 
 def makebutton(root, x, y, width, height, text, font, textcolor, bgcolor, activebackground, anchor, relief):
+    '''Deze functie maakt een knop op basis van de meegegeven variabelen en returnt deze voor verder
+    gebruik'''
     button = Button(root,
                     text=text,
                     font=font,
@@ -41,12 +47,16 @@ def makebutton(root, x, y, width, height, text, font, textcolor, bgcolor, active
 
 
 def switchbutton(clickedbutton, otherbutton, bgcolor, bgcoloractivated):
+    '''Deze functie zorgt ervoor dat er bij twee knoppen er altijd maar één knop ingedrukt is. Beide knoppen
+    mogen wel oningedrukt zijn'''
     togglebutton(clickedbutton, bgcolor, bgcoloractivated)
     if otherbutton['relief'] == 'sunken':
         togglebutton(otherbutton, bgcolor, bgcoloractivated)
 
 
 def togglebutton(button, bgcolor, bgcoloractivated):
+    '''Deze knop verandert de staat van de meegegeven knop. Als de meegegeven knop ingedrukt is zal deze
+    opgelaten worden en als de meegegeven knop opgelaten is zal deze ingedrukt blijven'''
     if button['relief'] == 'raised':
         button['relief'] = 'sunken'
         button['bg'] = bgcoloractivated
@@ -58,6 +68,8 @@ def togglebutton(button, bgcolor, bgcoloractivated):
 
 
 def makeentry(root, x, y, width, font, textcolor, bgcolor, anchor):
+    '''Deze functie maakt een invoerveld op basis van de meegegeven variabelen
+    en returnt deze voor verder gebruik'''
     entry = Entry(root,
                   font=font,
                   fg=textcolor,
@@ -68,6 +80,8 @@ def makeentry(root, x, y, width, font, textcolor, bgcolor, anchor):
 
 
 def makeframe(root, side, bgcolor, expand, fill, anchor):
+    '''Deze functie maakt een frame op basis van de meegegeven variabelen en
+    returnt deze voor verder gebruik'''
     frame = Frame(root,
                   bg=bgcolor)
     frame.pack(side=side, fill=fill, expand=expand, anchor=anchor)
@@ -75,6 +89,8 @@ def makeframe(root, side, bgcolor, expand, fill, anchor):
 
 
 def maketoplevel(root, windowwidth, windowheight, windowtitle, bgcolor):
+    '''Deze functie maakt een nieuw scherm bovenop een al bestaand scherm op basis van meegegeven
+    afmetingen en returnt deze voor verder gebruik'''
     subwindow = Toplevel(root)
     # De breedte en hoogte van het scherm ophalen
     screenwidth = root.winfo_screenwidth()
--- Recommendation/RecEngine.py
+++ Recommendation/RecEngine.py
@@ -4,16 +4,18 @@ import MongoInteract as M
 from tkinter import *
 from pymongo import MongoClient
 
-#algemene variabelen
+# algemene variabelen
 background = '#42b0f5'
 textcolor = 'black'
 bgbutton = 'white'
 bgbuttonactivated = 'gray'
-
-displayedcolumns = ('_id', 'name', 'brand', 'type', 'category', 'selling_price')
+# bij het toevoegen van kolommen moet de selling_price als laatste blijven; werkt niet bij nested values
+displayedcolumns = ('_id', 'name', 'brand', 'type', 'category', 'description', 'selling_price')
 
 
 def start():
+    '''Deze functie start het startscherm en zet alle elementen erin en verbindt deze met de juiste
+    acties'''
     root = G.makewindow(500, 700, 'Recommendations', background)
     generalframe = G.makeframe(root, TOP, background, 1, BOTH, 'n')
     productsframe = G.makeframe(root, BOTTOM, background, 1, BOTH, 's')
@@ -33,10 +35,13 @@ def start():
     submitbutton['command'] = lambda: submit(cursor, contentbutton, collaborativebutton, profileentry,
                                              submitbutton, productsframe, root)
     root.mainloop()
+    # connectie met PGAdmin database sluit wanneer het scherm wordt afgesloten
     P.closeconnection(connection, cursor)
 
 
 def submit(cursor, button1, button2, entry, submitbutton, frame, root):
+    '''Deze functie wordt geactiveerd wanneer de 'change' knop wordt geactiveerd. Deze functie zal
+    controleren welke knoppen zijn ingedrukt en zal dan de benodigde acties uitvoeren'''
     if button1['relief'] == 'sunken' or button2['relief'] == 'sunken':
         frame.destroy()
         frame = G.makeframe(root, BOTTOM, background, 1, BOTH, 's')
@@ -49,15 +54,20 @@ def submit(cursor, button1, button2, entry, submitbutton, frame, root):
 
 
 def setupconnection():
+    '''Deze functie maakt een connectie aan met de PGAdmin database en maakt daarna nog een cursor
+    voor interctie met deze database'''
     connection = P.databaseconnect('BusinessRules', 'postgres', 'broodje123')
     cursor = P.makecursor(connection)
     return connection, cursor
 
 
 def contentfilter(profileid, cursor, frame, root):
+    '''Deze functie recommend nieuwe producten op basis van de meest bekeken categorie van
+    het profiel dat op dit moment aangegeven staat'''
     check = checkprofileid(profileid, cursor)
     if check:
         bestcategory, alreadyviewed = getprofileinfo(cursor, profileid)
+        # maakt een filter om producten die al een keer bekeken zijn buiten beschouwing te laten
         filters = makefilter(alreadyviewed)
         thefilter = {'$and': [{'category': bestcategory}]+filters}
         client = MongoClient()
@@ -70,6 +80,9 @@ def contentfilter(profileid, cursor, frame, root):
 
 
 def collabfilter(profileid, cursor, frame, root):
+    '''Deze functie recommend nieuwe producten op basis van de producten die andere
+    profielen bekeken hebben met dezelfde favoriete categorie als het profiel dat
+    aangegeven staat'''
     check = checkprofileid(profileid, cursor)
     if check:
         bestcategory, alreadyviewed = getprofileinfo(cursor, profileid)
@@ -81,6 +94,7 @@ def collabfilter(profileid, cursor, frame, root):
         newproductquery = makefilter(alreadyviewed, newproductquery)
         newproductquery += f" limit 4"
         newproducts = P.getdata(cursor, newproductquery, False)
+        # maakt een filter om de met sql gevonden producten uit de MongoDB database te halen
         filters = makefilter(newproducts, exclude=False)
         thefilter = {'$or': filters}
         client = MongoClient()
@@ -93,16 +107,20 @@ def collabfilter(profileid, cursor, frame, root):
 
 
 def checkprofileid(profileid, cursor):
+    '''Deze functie gaat na of het ingevoerde profileid wel in de database staat'''
     result = P.getdata(cursor, f"select id from profiles where id='{profileid}'")
     return result is not None
 
 
 def handleerror(frame, text):
+    '''Deze funcie maakt een rode text voor wanneer er een error optreedt'''
     label = G.makelabel(frame, 0.05, 0.2, text, ('', 15, 'bold'),
                         'red', background, 'w')
 
 
 def getprofileinfo(cursor, profileid):
+    '''Deze functie haalt informatie op van het aangegeven profiel, zoals de favoriete categorie en de
+    producten die als bekeken zijn door dit profiel'''
     bestcategory = P.getdata(cursor, f"select bestcategory from bestcategories where profileid='{profileid}'")
     alreadyviewed = P.getdata(cursor, f"select prodid from profiles_previously_viewed where profid='{profileid}'",
                               False)
@@ -110,6 +128,8 @@ def getprofileinfo(cursor, profileid):
 
 
 def makefilter(products, query=False, exclude=True):
+    '''Deze functie maakt een filter voor sql of de pymongo library voor een groter aantal producten.
+    Deze functie kan zowel includeren als excluderen'''
     if query:
         for product in products:
             query += f" and not prodid='{product[0]}'"
@@ -126,6 +146,7 @@ def makefilter(products, query=False, exclude=True):
 
 
 def displayproducts(products, columns, frame, root):
+    '''Deze functie plaatst een rij van producten op het scherm als de recommendations'''
     fulldisplay = lambda x: (lambda: displayfullproduct(x, columns, root))
     number = 0
     for product in products:
@@ -139,6 +160,8 @@ def displayproducts(products, columns, frame, root):
 
 
 def displayfullproduct(product, columns, root):
+    '''Deze functie wordt geactiveerd zodra je op het 'expand' knopje drukt. Er wordt dan een nieuw
+    scherm gemaakt met uitgebreide informatie over het product'''
     newwindow = G.maketoplevel(root, 800, 400, product['name'], background)
     number = 0
     for column in columns:
