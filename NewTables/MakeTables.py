d9cee44fc0d6ecb4e8a42f32a4f5e20ffda1a58a
LukaHerrmann <lukajherrmann@gmail.com>
2021-03-18 14:53:08 +0100

    Een error in de tabel weggewerkt en commentregels toegevoegd.

--- NewTables/MakeTables.py
+++ NewTables/MakeTables.py
@@ -2,26 +2,39 @@ import PGInteract as P
 
 
 def profilecategories(connection, cursor):
+    '''Deze functie maakt een nieuwe tabel aan met daarin het profiel en de categorie die het meest bekeken is
+    door dit profiel'''
     print('Creating table')
+    # maakt een nieuwe table aan
     P.droptable(cursor, 'drop table if exists BestCategories CASCADE')
-    P.maketable(cursor, 'create table BestCategories(profileid varchar, bestcategory varchar,'
+    P.maketable(cursor, 'create table BestCategories(profileid varchar, bestcategory varchar, frequency int, '
                         'foreign key (profileid) references profiles(id))')
     connection.commit()
     print('Getting data')
-    profilequery = 'select profid, category, max(frequency) as highestfreq from( ' \
+    # sql query die de nieuwe informatie genereert
+    # bestaat uit twee gegenereerde tables die zijn samengevoegd
+    # de eerste table pakt de hoogste frequency van de categorie die voorkomt
+    # de tweede table heeft de frequencies van alle categorieen van de bekenen producten
+    profilequery = 'SELECT a.profid, b.category, a.highestfreq ' \
+                   'FROM (select profid, max(frequency) as highestfreq from(' \
                    'select profid, category, count(category) as frequency ' \
-                    'from profiles_previously_viewed ' \
-                    'inner join products on prodid = id ' \
-                    'group by category, profid) as subsub ' \
-                    'group by profid, category ' \
-                    'order by highestfreq ASC '
+                   'from profiles_previously_viewed ' \
+                   'inner join products on prodid = id ' \
+                   'group by profid, category) as subsub1 ' \
+                   'group by profid) as a ' \
+                   'INNER JOIN (select profid, category, count(category) as frequency ' \
+                   'from profiles_previously_viewed inner join products on prodid = id ' \
+                   'group by profid, category) as b ' \
+                   'ON a.profid = b.profid AND a.highestfreq = b.frequency ' \
+                   'order by highestfreq ASC'
     profileinfo = P.getdata(cursor, profilequery, False)
     totalsize = len(profileinfo)
     iteration = 0
     percentile = 0
     print('Inserting data')
+    # zet de gegenereerde data in de nieuwe table
     for profile in profileinfo:
-        insertquery = f"insert into BestCategories values ('{profile[0]}','{profile[1]}')"
+        insertquery = f"insert into BestCategories values ('{profile[0]}', '{profile[1]}', '{profile[2]}')"
         P.insertdata(cursor, insertquery)
         iteration += 1
         if iteration/totalsize*100 >= percentile:
