91b1035b694ed217f31a0b8f3e9d388aa0a91f05
LukaHerrmann <lukajherrmann@gmail.com>
2021-03-18 01:39:45 +0100

    Programma gemaakt voor het ophalen van items uit de MongoDB.

---
+++ General/MongoInteract.py
@@ -0,0 +1,9 @@
+
+def getitems(collection, filter, limit=False, sortterm=False):
+    '''Deze functie haalt alle items op uit een MongoDB collection'''
+    result = collection.find(filter)
+    if sortterm:
+        result = result.sort(sortterm)
+    if limit:
+        result = result.limit(limit)
+    return result
